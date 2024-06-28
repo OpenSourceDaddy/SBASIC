f4dfb889589a5bbe9cfeeb5567f0e1a9cfe60dc8
OpenSourceDaddy <167347916+OpenSourceDaddy@users.noreply.github.com>
2024-06-28 20:28:43 +0530

    Updated code for main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,9 @@
 #include <string>
 #include <vector>
 #include <regex>
-
+#include "internal/readFile.h"
 using namespace std;
 
-vector<string> readFile();
 vector<string> Lex(const vector<string>& fileContents);
 void parse(vector<string> tok);
 bool isNumber(const std::string& str);
@@ -14,32 +13,17 @@ bool isNumber(const std::string& str);
 int main() {
     vector<string> fileContents = readFile();
     vector<string> tokens = Lex(fileContents);
-    parse(tokens);
+
+    for (const auto& token : tokens) {
+        cout << token << endl;
+    }
+    //parse(tokens);
 
     return 0;
 }
 
-vector<string> readFile(){
-    std::ifstream in("test.sbs", std::ios_base::in);
-    if (!in) {
-        cerr << "Error opening the file." << endl;
-        return {};
-    }
-    std::string line;
-    vector<string> lines;
-    while (getline(in, line)) {
-        lines.push_back(line);
-    }
-    lines.push_back("<EOF>");
-    in.close();
-    return lines;
-}
-
 bool isNumber(const std::string& str) {
-    // Define the regex pattern to match exactly one digit from 0 to 9
     std::regex pattern("^[0-9]+$");
-
-    // Check if the string matches the pattern
     return std::regex_match(str, pattern);
 }
 
@@ -61,27 +45,28 @@ vector<string> Lex(const vector<string>& fileContents) {
                     tok = " ";
                 }
             } else if (tok == "\n" || tok == "<EOF>") {
-                if(expr != "" && isexpr==true){
-                    tokens.push_back("EXPR:"+expr);
-                    expr="";
-                    //isexpr=false;
-                }else if(expr !="" && isexpr==false){
-                    tokens.push_back("NUM:"+expr);
-                    expr="";
+                if (expr != "" && isexpr == false) {
+                    tokens.push_back("NUM:" + expr);
+                    expr = "";
                 }
+                else if (expr != "" && isexpr == true) {
+                    tokens.push_back("EXPR:" + expr);
+                    expr = "";
+                    isexpr = false;
+                }
+                tokens.push_back("<EOF>");
                 tok = "";
             } else if (tok == "PRINT" || tok == "print") {
                 tokens.push_back("print");
                 tok = "";
-            }else if(isNumber(tok)){
-                //cout << "NUMBER" << endl;
+            } else if (isNumber(tok)) {
                 expr += tok;
-                tok="";
-            }else if (tok=="+"){
+                tok = "";
+            } else if (tok == "+" || tok=="-" || tok=="/" || tok=="*") {
                 isexpr = true;
-                expr += "+";
-                tok="";
-            }else if (tok == "\"") {
+                expr += tok;
+                tok = "";
+            } else if (tok == "\"") {
                 if (state == 0) {
                     state = 1;
                 } else if (state == 1) {
@@ -90,31 +75,26 @@ vector<string> Lex(const vector<string>& fileContents) {
                     state = 0;
                     tok = "";
                 }
-
             } else if (state == 1) {
                 String += tok;
                 tok = "";
             }
         }
     }
-    //cout << expr << endl;
     return tokens;
 }
 
 void parse(vector<string> tok) {
-    int i = 0;
-    while (i < tok.size() - 1) {
-        cout << tok[i] + "" + tok[i+1] << endl;
-        if((tok[i]+" "+ tok[i+1].substr(0,3))=="PRINT NUM" || (tok[i]+" "+tok[i+1].substr(0,3))=="print NUM"){
-            cout << tok[i+1].substr(4,tok[i+1].size()) << endl;
+    for (size_t i = 0; i < tok.size(); ++i) {
+        if (tok[i] == "print") {
+            if (tok[i + 1].substr(0, 6) == "STRING") {
+                cout << "STRING:" + tok[i + 1].substr(7, tok[i + 1].size() - 1) << endl;
+            } else if (tok[i + 1].substr(0, 3) == "NUM") {
+                cout << "NUM:" + tok[i + 1].substr(4, tok[i + 1].size() - 4) << endl;
+            } else if (tok[i + 1].substr(0, 4) == "EXPR") {
+                cout << "EXPR:" + tok[i + 1].substr(5, tok[i + 1].size() - 5) << endl;
+            }
             i+=2;
-        }else if((tok[i]+" " +tok[i+1].substr(0,4))=="PRINT EXPR" || (tok[i]+" " +tok[i+1].substr(0,4))=="print EXPR"){
-            cout << tok[i+1].substr(5,tok[i+1].size()) << endl;
-            i +=2;
-        }
-        else if ((tok[i] + " " + tok[i + 1].substr(0, 6)) == "PRINT STRING" || (tok[i]+" "+tok[i+1].substr(0,6))=="print STRING") {
-            cout << tok[i+1].substr(7,tok[i+1].size()) << endl;
-            i += 2;
         }
     }
 }
