f3674725e5b41567ccb19f9becc37100d96787cd
OpenSourceDaddy <167347916+OpenSourceDaddy@users.noreply.github.com>
2024-06-28 20:29:18 +0530

    Added readFile.h

--- internal/readFile.h
+++ internal/readFile.h
@@ -1,1 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
 
+using namespace std;
+
+vector<string> readFile(){
+    std::ifstream in("test.sbs", std::ios_base::in);
+    if (!in) {
+        cerr << "Error opening the file." << endl;
+        return {};
+    }
+    std::string line;
+    vector<string> lines;
+    while (getline(in, line)) {
+        lines.push_back(line);
+    }
+    lines.push_back("<EOF>");
+    in.close();
+    return lines;
+}
